0694941689f41e78ed09e70c8606705559f195ea
Pietro Panta <pietrofernandosp@gmail.com>
2026-05-11 12:38:48 +0000

    testando o sistema HTLM

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 function Home() {
-    return <h1>Testando mecânicas do git. </h1>
+    return <h1>Eae gurizada!!. </h1>
+    return <h2> Testando o sistema HTML</h2>
+    return <h3>Para ver se é o bixo mesmo</h3>
+    return <h5>Bora criar um site?? 😏</h5>
 }
 
 export default Home;
