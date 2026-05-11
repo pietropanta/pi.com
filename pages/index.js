4dc6de3e87c6d3ea072ff50f8f63d8b777a91857
Pietro Panta <pietrofernandosp@gmail.com>
2026-05-11 12:48:01 +0000

    Agora foi eu que errei xD

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 function Home() {
-    return <h1>Eae gurizada!!. </h1>
-    return <h2> Testando o sistema HTML</h2>
-    return <h3>Para ver se é o bixo mesmo</h3>
-    return <h5>Bora criar um site?? 😏</h5>
+    <return> 
+        <h1>Eae gurizada!! </h1>
+    <h2> Testando o sistema HTML</h2>
+    <h3>Para ver se é o bixo mesmo</h3>
+    <h5>Bora criar um site?? 😏</h5>
+    </return>
 }
 
 export default Home;
