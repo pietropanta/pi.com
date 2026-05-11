a873b788b4cb0e0a1ec4b223f5bb4aea184a3a05
Pietro Panta <pietrofernandosp@gmail.com>
2026-05-11 12:53:05 +0000

    Bah, não sei fazer HTML 😒

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,5 @@
 function Home() {
-    <return> 
-        <h1>Eae gurizada!! </h1>
-    <h2> Testando o sistema HTML</h2>
-    <h3>Para ver se é o bixo mesmo</h3>
-    <h5>Bora criar um site?? 😏</h5>
-    </return>
+    return <h2> Testando o sistema HTML 🐊</h2>
 }
 
 export default Home;
